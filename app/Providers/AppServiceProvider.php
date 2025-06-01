218130abae9feafc8ea87b0756a77308e6495b78
Magicfenixx <magicfenixx@gmail.com>
2025-06-01 21:40:02 +0300

    reset password link fixed

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,7 +21,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
          ResetPassword::createUrlUsing(function ($notifiable, $token) {
-        return config('app.url') . '/reset-password?token=' . $token . '&email=' . urlencode($notifiable->getEmailForPasswordReset());
+        return config('app.frontend_url','http://localhost:5173') . '/reset-password?token=' . $token . '&email=' . urlencode($notifiable->getEmailForPasswordReset());
         });
     }
 }
